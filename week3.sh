e317c25d0284e5cac1def0ca557052be0705a62a
Rony Valle Lopez <35381394+RonyValle@users.noreply.github.com>
2018-05-08 13:48:14 -0500

    Update week3.sh

--- week3.sh
+++ week3.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 clear
+# sending a sentence to the dev/null directory.
+echo this is going nowhere > dev/null
+#listing the block id
+blkid
+#listing the block devices in your computer
+lsblk
+#using grep to list block devices
+ls -l /dev /dev/mapper | grep '^b'
+#checking for an sd in the dmesg using grep
+dmesg | grep sd
